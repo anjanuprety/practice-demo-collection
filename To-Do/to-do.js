3d08f365c089b537a724bd7383c8fdcd680383a9
Anjan Upreti <anjanuprety787@gmail.com>
2024-09-11 12:19:47 +0545

    Just storing

---
+++ To-Do/to-do.js
@@ -0,0 +1,8 @@
+const toDoList=[];
+    function addTodo(){
+      const inputElement= document.querySelector('.js-name-input');
+      const name= inputElement.value;
+      toDoList.push(name);
+      console.log(toDoList);
+      inputElement.value='';
+    }
